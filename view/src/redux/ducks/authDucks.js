2ba6006a00a90654f781f00d1eb671ddf50309dd
Josue Davalos <jedavalo@espol.edu.ec>
2021-07-10 11:28:38 -0500

    Set Prettier for formatting and ESlint rules

--- view/src/redux/ducks/authDucks.js
+++ view/src/redux/ducks/authDucks.js
@@ -3,69 +3,69 @@ import { setCokies } from "../../components/utils/auth";
 import { root } from "../../components/utils/constants";
 
 const data = {
-	auth: false,
-	token: "",
-	user: {},
+  auth: false,
+  token: "",
+  user: {},
 };
 
 const GET_AUTH = "GET_AUTH";
 
 export default function authReducer(state = data, action) {
-	switch (action.type) {
-		case GET_AUTH:
-			var userLogIn = {
-				auth: action.payload.auth,
-				token: action.payload.token,
-				user: action.payload.user,
-			};
-			if (userLogIn.auth) {
-				setCokies(userLogIn);
-				window.location.href = root + "/panel";
-			} else {
-				alert("Usuario o Contaseña Incorrectos");
-			}
-			return {
-				...state,
-				auth: action.payload.auth,
-				token: action.payload.token,
-				user: action.payload.user,
-			};
-		default:
-			return state;
-	}
+  switch (action.type) {
+    case GET_AUTH:
+      var userLogIn = {
+        auth: action.payload.auth,
+        token: action.payload.token,
+        user: action.payload.user,
+      };
+      if (userLogIn.auth) {
+        setCokies(userLogIn);
+        window.location.href = root + "/panel";
+      } else {
+        alert("Usuario o Contaseña Incorrectos");
+      }
+      return {
+        ...state,
+        auth: action.payload.auth,
+        token: action.payload.token,
+        user: action.payload.user,
+      };
+    default:
+      return state;
+  }
 }
 
 export const getAuthAction = (authObject) => async (dispatch) => {
-	const dataLogIn = {
-		email: authObject.username,
-		password: authObject.password,
-	};
+  const dataLogIn = {
+    email: authObject.username,
+    password: authObject.password,
+  };
 
-	// eslint-disable-next-line no-undef
-	await fetch(ENDPOINT_AUTH, {
-		method: "POST",
-		headers: {
-			Accept: "application/json",
-			"Content-Type": "application/json",
-		},
-		body: JSON.stringify(dataLogIn),
-	})
-		.then((res) => {
-			return res.json();
-		})
-		.then((res) => {
-			dispatch({
-				type: GET_AUTH,
-				payload: res,
-			});
-			// if (res.auth) {
-			//   // navigation.navigate("Panel", { token: res.token, correo: email });
-			//   console.log("1");
-			// } else {
-			//   console.log("2");
+  // eslint-disable-next-line no-undef
+  await fetch(ENDPOINT_AUTH, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(dataLogIn),
+  })
+    .then((res) => {
+      return res.json();
+    })
+    .then((res) => {
+      dispatch({
+        type: GET_AUTH,
+        payload: res,
+      });
+      // if (res.auth) {
+      //   // navigation.navigate("Panel", { token: res.token, correo: email });
+      //   console.log("1");
+      // } else {
+      //   console.log("2");
 
-			//   // Alert.alert("Error de Inicio de Sesión", res.message);
-			// }
-		})
-		.catch((err) => console.log("Error:", err));
+      //   // Alert.alert("Error de Inicio de Sesión", res.message);
+      // }
+    })
+    .catch((err) => console.log("Error:", err));
 };

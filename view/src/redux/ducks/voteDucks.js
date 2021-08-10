e332623de2c6cd5d2430a1a74384a6f29a0d4ebc
Josue Davalos <jedavalo@espol.edu.ec>
2021-08-10 10:49:41 -0500

    Improve VoteDucks

--- view/src/redux/ducks/voteDucks.js
+++ view/src/redux/ducks/voteDucks.js
@@ -5,31 +5,16 @@ const data = {
   array: [],
 };
 
-const GET_VOTES = "GET_VOTES";
 const SEND_VOTE = "SEND_VOTE";
 
 export default function voteReducer(state = data, action) {
   switch (action.type) {
-    case GET_VOTES:
-      return { ...state, array: action.payload };
     case SEND_VOTE:
       return state;
     default:
       return state;
   }
 }
-
-export const getVotesAction = () => async (dispatch) => {
-  try {
-    const res = await axios.get(ENDPOINT_VOTE);
-    dispatch({
-      type: GET_VOTES,
-      payload: res.data,
-    });
-  } catch (error) {
-    console.log(error);
-  }
-};
 
 export const sendVoteAction =
   (dataset, label, user, game, knowledgeLevel) => async (dispatch) => {

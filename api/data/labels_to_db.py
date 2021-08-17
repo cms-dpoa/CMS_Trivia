3de509b8b9b9c9de6d833b18d2de45997350c3d9
Josue Davalos <jedavalo@espol.edu.ec>
2021-08-16 19:24:16 -0500

    Add data to DB

--- api/data/labels_to_db.py
+++ api/data/labels_to_db.py
@@ -1,18 +1,21 @@
-import sqlite3
 import pandas as pd
-import numpy as np
+import pymysql
+
+conextion = pymysql.connect(user='root', 
+                            password='root',
+                            host='localhost',
+                            database='cms_trivia')
+cursor = conextion.cursor()
+
 
 labels = pd.read_csv('Label.txt', delimiter = '\t')
 ids = list(labels['id_label'].values)
 names = list(labels['name'].values)
 was_created = list(labels['was_created'].values)
 
-con = sqlite3.connect('db.sqlite3')
-c = con.cursor()
-
 for i in range(len(ids)):
     command = f"INSERT INTO api_label VALUES({ids[i]}, '{names[i]}', {was_created[i]})"
-    c.execute(command)
+    cursor.execute(command)
 
-con.commit()
-con.close()
+conextion.commit()
+conextion.close()
